07bbf11cff681f0ce4c8a2b29f91a0fc335c287f
Dineshkumarraman <31721668+Dineshkumarraman@users.noreply.github.com>
2017-09-15 13:09:33 +0530

    colon removed

--- index.js
+++ index.js
@@ -141,4 +141,4 @@ app.post('/validate', function(req,res){
   app.listen(process.env.PORT || 1337, () => {
     console.log('listening on 1337')
   })
-// app.listen(process.env.PORT || 1337);;
+// app.listen(process.env.PORT || 1337);
